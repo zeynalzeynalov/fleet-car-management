913bf6f868396636bcd4f61ab36c55c38e7f22ba
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2022-10-31 19:24:33 +0100

    Zeynal | Add Car domain model

---
+++ src/main/java/com/happyfleet/app/api/cars/Car.java
@@ -0,0 +1,32 @@
+package com.happyfleet.app.api.cars;
+
+import com.happyfleet.app.core.AuditableWithId;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "car",
+        uniqueConstraints = {@UniqueConstraint(columnNames = "licensePlate")})
+public class Car extends AuditableWithId<String> {
+
+    @Column(nullable = false, length = 15)
+    private String licensePlate;
+
+    // TODO: following properties can be normalized as new table: brand, manufacturer, operationCity, status
+    @Column(nullable = false, length = 15)
+    private String brand;
+
+    @Column(nullable = false, length = 15)
+    private String manufacturer;
+
+    @Column(nullable = false, length = 15)
+    private String operationCity;
+
+    @Enumerated(EnumType.STRING)
+    private CarStatus status;
+}
