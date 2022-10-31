588c3e7c3c659567a393e98b2afad5e94390d1ed
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2022-10-31 17:08:50 +0100

    Zeynal | Initial commit for empty project
    Zeynal | Initial commit for empty project

---
+++ src/main/java/com/happyfleet/app/FleetCarManagementApplication.java
@@ -0,0 +1,13 @@
+package com.happyfleet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetCarManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetCarManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyfleet/app/FleetCarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyfleet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetCarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
